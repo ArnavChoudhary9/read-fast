a23ce6f06f6dc9d2c09c4d2577072b570f9cd3d0
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-04-17 16:35:26 +0530

    TypeWrite Animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,46 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
-export default function Home() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isHovered, setIsHovered] = useState(false);
+const Home = () => {
+  const [testBegun, setTestBegun] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    setPosition({
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top,
-    });
-  };
+  useEffect(() => {
+    if (testBegun) {
+      setTimeout(() => { setShowButton(false) }, 300);
+    } else {
+      setShowButton(true);
+    }
+  }, [testBegun])
 
   return (
-    <div className="flex items-center justify-center min-h-screen animated-gradient relative">
-      <div
-        className="relative"
-        onMouseMove={isHovered ? handleMouseMove : undefined}
-      >
+    <div className="flex flex-col items-center justify-center min-h-screen animated-gradient relative">
+      <TypeAnimation
+        sequence={
+          ['Test how fast can you read!', 1000]
+        }
+        wrapper="span"
+        speed={75}
+        className={`font-mono text-3xl text-white p-7 transition-transform duration-300 ease-in-out ${testBegun ? 'translate-y-10' : 'translate-y-0'
+          }`}
+      />
+
+      {showButton && (
         <button
           className="rounded-full border font-thin text-3xl bg-transparent px-6 py-3 hover:scale-105 transition-transform duration-300 ease-in-out overflow-hidden border-white text-white relative z-10"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          onMouseMove={handleMouseMove}
+          onClick={() => setTestBegun(true)}
+          style={{
+            transform: testBegun ? 'scale(0)' : 'scale(1)',
+            transition: 'transform 0.3s ease-in-out',
+          }}
         >
-          Test
-
-          {isHovered && (
-            <div
-              className="absolute rounded-full pointer-events-none"
-              style={{
-                width: "150px",
-                height: "150px",
-                background: `radial-gradient(circle, rgba(255, 182, 193, 1) 0%, rgba(0, 255, 255, 0.1) 50%)`,
-                transform: `translate(-50%, -50%)`,
-                top: `${position.y}px`,
-                left: `${position.x}px`,
-                zIndex: -1,
-              }}
-            />
-          )}
+          Start
         </button>
-      </div>
+      )}
     </div>
   );
 }
+
+export default Home;
